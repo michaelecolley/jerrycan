03b182d8a17f4b82379721afe473f9959b43e214
michaelecolley <michael.e.colley@googlemail.com>
2021-12-11 00:39:41 +0000

    Refactor polygon provider tests

--- __tests__/polygon.provider.ts
+++ __tests__/polygon.provider.ts
@@ -1,45 +1,55 @@
 import PolygonProvider from "../src/providers/polygon.provider";
-
+import { spyConsole } from "./spyConsole";
+const nock = require("nock");
 describe("Testing the Polygon Gas Service", () => {
-  describe("Function", () => {
-    it("Returned Object is not null", async () => {
-      const gasPriceService = await new PolygonProvider();
-      const gasPrice = await gasPriceService.getLatest();
+  describe("Check class functionality", () => {
+    let gasPrice;
+    const url = "https://gasstation-mainnet.matic.network";
 
+    beforeAll(async () => {
+      const scope = nock(url).get("/").reply(200, {
+        safeLow: 36,
+        standard: 50,
+        fast: 228,
+        fastest: 228,
+        blockTime: 2,
+        blockNumber: 22376333,
+      });
+      const gasPriceService = await new PolygonProvider();
+      gasPrice = await gasPriceService.getLatest();
+      console.log(gasPrice);
+    });
+    it("Returned Object is not null", async () => {
       expect(gasPrice).not.toBeNull();
     });
 
     it("Type check output Object to contain target value datatype: number", async () => {
-      const testObject = {};
-      const gasPriceService = await new PolygonProvider();
-      const gasPrice = await gasPriceService.getLatest();
-      if (gasPrice != null) {
-        if (gasPrice.maxPriorityFeePerGas != null) {
-          testObject["maxFeePerGas"] = gasPrice.maxFeePerGas;
-          testObject["maxPriorityFeePerGas"] = gasPrice.maxPriorityFeePerGas;
-          expect(typeof testObject["maxFeePerGas"]).toBe("number");
-        } else {
-          testObject["gasPrice"] = gasPrice.price;
-          expect(typeof testObject["gasPrice"]).toBe("number");
-        }
-      }
+      expect(typeof gasPrice["price"]).toBe("number");
     });
 
     it("GetLatest function call returns a single key value pair: the price and a number more than 0", async () => {
-      const testObject = {};
-      const gasPriceService = await new PolygonProvider();
-      const gasPrice = await gasPriceService.getLatest();
-      if (gasPrice != null) {
-        if (gasPrice.maxPriorityFeePerGas != null) {
-          testObject["maxFeePerGas"] = gasPrice.maxFeePerGas;
-          testObject["maxPriorityFeePerGas"] = gasPrice.maxPriorityFeePerGas;
-        } else {
-          testObject["gasPrice"] = gasPrice.price;
-        }
-        expect(testObject).toHaveProperty("gasPrice");
-        expect(Object.keys(testObject).length).toBe(1);
-        expect(testObject["gasPrice"] > 0).toBe(true);
-      }
+      expect(gasPrice).toHaveProperty("price");
+      expect(Object.keys(gasPrice).length).toBe(1);
+      expect(gasPrice["price"] > 0).toBe(true);
+    });
+  });
+  describe("Check error handling", () => {
+    let gasPrice;
+    const spy = spyConsole();
+    beforeAll(async () => {
+      nock.disableNetConnect();
+    });
+
+    it("Test that correct error is thrown when service can't connect to external API", async () => {
+      let gasPriceService = await new PolygonProvider();
+      gasPrice = await gasPriceService.getLatest();
+      expect(console.error).toHaveBeenCalledTimes(1);
+      expect(spy.console["mock"]["calls"][0]).toContain(
+        "[Polygon] Gas Platform Error"
+      );
+    });
+    it("Test that null is returned from gasPrice variable when no API call is successfully made", async () => {
+      expect(gasPrice).toBeNull();
     });
   });
 });
