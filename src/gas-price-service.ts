5f4e603fffbae270157c4079a2ec34d5ec113c3a
michaelecolley <michael.e.colley@googlemail.com>
2021-12-11 02:01:11 +0000

    add docomments to gas price service

--- src/gas-price-service.ts
+++ src/gas-price-service.ts
@@ -3,6 +3,14 @@ import PolygonProvider from "./providers/polygon.provider";
 import { GasPrice } from "./types/types";
 
 export default class GasPriceService {
+  /**
+   *
+   * Returns the gas price recommendations depending on the input network key
+   *
+   * @param networkKey String value networkKey that corresponds to a given eth gas price estimator provider
+   * @returns A gas price estmation in GWei
+   *
+   */
   constructor(
     private readonly blocknativeProvider = new BlocknativeProvider(),
     private readonly polygonProvider = new PolygonProvider()
