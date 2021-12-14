9aabed4a93290ba458909d35701ffd33925f6833
michaelecolley <michael.e.colley@googlemail.com>
2021-12-14 23:28:20 +0000

    Add type fixings into gas-price-service

--- src/gas-price-service.ts
+++ src/gas-price-service.ts
@@ -2,36 +2,54 @@ import BlocknativeProvider from './providers/blocknative.provider';
 import PolygonProvider from './providers/polygon.provider';
 import { GasPrice } from './types/types';
 
+type Params = {
+  blocknativeApiKey?: string | null;
+  ethGasApiKey?: string | null;
+};
+
+const params: Params = {};
+
+enum Provider {
+  blocknative = 'BLOCKNATIVE',
+  polygon = 'POLYGON'
+}
+
+enum Network {
+  EthereumMainet = '1',
+  Polygon = '137'
+}
+
 export default class GasPriceService {
-  /**
-   *
-   * Returns the gas price recommendations depending on the input network key
-   *
-   * @param networkKey String value networkKey that corresponds to a given eth gas price estimator provider
-   * @returns A gas price estmation in GWei
-   *
-   */
   constructor(
-    blockNativeProvidedApiKey?: string,
+    { blocknativeApiKey = null, ethGasApiKey = null } = params,
     private readonly blocknativeProvider = new BlocknativeProvider(
-      blockNativeProvidedApiKey
+      blocknativeApiKey
     ),
     private readonly polygonProvider = new PolygonProvider()
   ) {}
 
-  public async getLatest(configService?: string): Promise<GasPrice | null> {
-    console.log(`configService in use: ${configService}`);
-    switch (configService) {
-      case '1':
-        return await this.blocknativeProvider.getLatest();
-      case 'blocknative':
-        return await this.blocknativeProvider.getLatest();
-      case '137':
-        return await this.polygonProvider.getLatest();
-      case 'polygon':
-        return await this.polygonProvider.getLatest();
-      default:
-        return null;
+  public async getLatest(
+    chainId: string,
+    provider: string
+  ): Promise<GasPrice | null> {
+    /**
+     *
+     * Returns the gas price recommendations depending on the input network key
+     *
+     * @param chainId String value chainId that corresponds to a given eth gas price estimator provider
+     * @returns A gas price estmation in GWei
+     *
+     */
+    console.log(`configService in use: ${chainId}`);
+    if (chainId == Network.EthereumMainet && provider == Provider.blocknative) {
+      return await this.blocknativeProvider.getLatest();
+    } else if (chainId == Network.Polygon && provider == Provider.polygon) {
+      return await this.polygonProvider.getLatest();
+    } else {
+      console.error(
+        'User input error: Pass in a supported chainId and corresponding provider name'
+      );
+      return null;
     }
   }
 }
