61cc58a703c56675d43a0852798df4cdb8ad8611
michaelecolley <michael.e.colley@googlemail.com>
2021-12-15 03:09:57 +0000

    Rename gasPriceService

--- src/gasPriceService.ts
+++ src/gasPriceService.ts
@@ -1,17 +1,19 @@
 import BlocknativeProvider from './providers/blocknative.provider';
 import PolygonProvider from './providers/polygon.provider';
+import EthGasStationProvider from './providers/ethGasStation.provider';
 import { GasPrice } from './types/types';
 
 type Params = {
   blocknativeApiKey?: string | null;
-  ethGasApiKey?: string | null;
+  ethGasStationApiKey?: string | null;
 };
 
 const params: Params = {};
 
 enum Provider {
   blocknative = 'BLOCKNATIVE',
-  polygon = 'POLYGON'
+  polygon = 'POLYGON',
+  ethGasStation = 'ETHGASSTATION'
 }
 
 enum Network {
@@ -21,9 +23,12 @@ enum Network {
 
 export default class GasPriceService {
   constructor(
-    { blocknativeApiKey = null, ethGasApiKey = null } = params,
+    { blocknativeApiKey = null, ethGasStationApiKey = null } = params,
     private readonly blocknativeProvider = new BlocknativeProvider(
       blocknativeApiKey
+    ),
+    private ethGasStationProvider = new EthGasStationProvider(
+      ethGasStationApiKey
     ),
     private readonly polygonProvider = new PolygonProvider()
   ) {}
@@ -36,20 +41,25 @@ export default class GasPriceService {
      *
      * Returns the gas price recommendations depending on the input network key
      *
-     * @param chainId String value chainId that corresponds to a given eth gas price estimator provider
+     * @param chainId String value chainId that corresponds to a given ethereum network chain id
+     * @param provider String value provider that corresponds to a given eth gas price estimator provider
      * @returns A gas price estmation in GWei
      *
      */
-    console.log(`configService in use: ${chainId}`);
-    if (chainId == Network.EthereumMainet && provider == Provider.blocknative) {
-      return await this.blocknativeProvider.getLatest();
+    console.log(`ChainId in use: ${chainId}`);
+    if (chainId == Network.EthereumMainet) {
+      if (provider == Provider.blocknative) {
+        return await this.blocknativeProvider.getLatest();
+      } else if (provider == Provider.ethGasStation) {
+        return await this.ethGasStationProvider.getLatest();
+      }
     } else if (chainId == Network.Polygon && provider == Provider.polygon) {
       return await this.polygonProvider.getLatest();
     } else {
       console.error(
-        'User input error: Pass in a supported chainId and corresponding provider name'
+        '[User input error: Pass in a supported chainId and corresponding provider name'
       );
-      return null;
     }
+    return null;
   }
 }
